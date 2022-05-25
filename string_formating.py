b1deab7211de29e214e07d6b14b75fe84c299db3
Your Name <you@example.com>
2022-05-25 15:32:05 +0600

    add new

---
+++ string_formating.py
@@ -0,0 +1,16 @@
+print('I am %s' % 'akil')
+print('I am %s and I am %d years old' % ('akil',23))
+
+name ="mark"
+age =34
+profession = 'player'
+earning = 23.56
+
+print('I am %s and I am %d years old. I am a %s and I earn %.3f$' % (name, age,profession, earning))
+
+print('I am {} and I am {}'.format('akil','happy'))
+print('I am {1} and I am {0}'.format('akil', 'happy'))
+
+food ="barger"
+drink = "tea"
+print(f'My name {name}, I am {age} years old, I like {food} and I eat {drink}')
